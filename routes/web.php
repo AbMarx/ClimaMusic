00a8470b29a8408d411e5f277ff86b2e645d860c
Abner Marques de Oliveira <abner.oliveira-plu@parceiros-kroton.com.br>
2020-06-28 18:46:28 -0300

    ajustes finais

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,6 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-$router->get('/', function(){
-    return redirect("http://documentation.climamusic.kinghost.net");
-});
 
-$router->get('api/weather/{city}', 'weatherConsultationsController@getMusicSuggestion');
-$router->get('api/statistics', 'weatherConsultationsController@getCityMetrics');
+$router->get('weather/{city}', 'weatherConsultationsController@getMusicSuggestion');
+$router->get('statistics', 'weatherConsultationsController@getCityMetrics');
